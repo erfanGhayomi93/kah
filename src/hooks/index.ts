87280edd9fbccbbbc703b1bdefaf9a470843d054
Ali Yaghoubi <dev.edwardjoseph@gmail.com>
2023-12-20 11:41:06 +0330

    Add "useInputs" and "useErrors"

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,6 @@
 import useDebounce from './useDebounce';
+import useErrors from './useErrors';
+import useInputs from './useInputs';
 import useLocalstorage from './useLocalstorage';
 
-export {
-	useDebounce,
-	useLocalstorage
-};
+export { useDebounce, useErrors, useInputs, useLocalstorage };
