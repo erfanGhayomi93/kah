d3beba96aa9a56243c5d576b8f8a83e0d914d214
Ali Yaghoubi <dev.edwardjoseph@gmail.com>
2023-12-20 11:40:55 +0330

    Split "AuthenticationModal"

--- src/components/modals/AuthenticationModal/index.tsx
+++ src/components/modals/AuthenticationModal/index.tsx
@@ -1,10 +1,9 @@
-import { ArrowLeftSVG, XSVG } from '@/components/icons';
+import { XSVG } from '@/components/icons';
 import { useAppDispatch } from '@/features/hooks';
 import { toggleAuthenticationModal } from '@/features/slices/modalSlice';
-import { convertStringToNumber } from '@/utils/helpers';
-import { useState } from 'react';
 import styled from 'styled-components';
 import Modal from '../Modal';
+import PhoneNumberForm from './PhoneNumberForm';
 
 const Div = styled.div`
 	width: 578px;
@@ -12,17 +11,10 @@ const Div = styled.div`
 `;
 
 const AuthenticationModal = () => {
-	const [phoneNumber, setPhoneNumber] = useState('');
-
 	const dispatch = useAppDispatch();
 
 	const onCloseModal = () => {
 		dispatch(toggleAuthenticationModal(false));
-	};
-
-	const onChangeInput = (value: string) => {
-		if (value.length > 12) return;
-		setPhoneNumber(value);
 	};
 
 	return (
@@ -38,24 +30,7 @@ const AuthenticationModal = () => {
 					<h1 className='text-3xl font-bold text-gray-100'>ورود به کهکشان</h1>
 				</div>
 
-				<form method='get' className='flex flex-col gap-88 px-64 pt-96'>
-					<label className='relative flex flex-col gap-8'>
-						<span className='text-base font-medium text-gray-100'>شماره همراه</span>
-						<input
-							type='text'
-							inputMode='numeric'
-							className='bg-transparent ltr h-48 rounded border border-gray-300 px-16 text-right'
-							placeholder='شماره همراه خود را وارد کنید'
-							value={phoneNumber}
-							onChange={(e) => onChangeInput(convertStringToNumber(e.target.value))}
-						/>
-					</label>
-
-					<button type='submit' className='btn-primary h-48 gap-4 rounded shadow'>
-						ادامه
-						<ArrowLeftSVG />
-					</button>
-				</form>
+				<PhoneNumberForm />
 			</Div>
 		</Modal>
 	);
--- src/components/modals/Modals.tsx
+++ src/components/modals/Modals.tsx
@@ -6,7 +6,7 @@ import AuthenticationModal from './AuthenticationModal';
 const Modals = () => {
 	const { authenticationModal } = useAppSelector((state) => state.modal);
 
-	return [authenticationModal && <AuthenticationModal />];
+	return [authenticationModal && <AuthenticationModal key='authentication-modal' />];
 };
 
 export default Modals;
