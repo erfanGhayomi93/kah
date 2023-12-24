fe90742e62e4ab839e036d51fa5f8417a965b7be
Ali Yaghoubi <dev.edwardjoseph@gmail.com>
2023-12-24 17:48:22 +0330

    Add "usePagination" hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,5 +3,6 @@ import useDebounce from './useDebounce';
 import useErrors from './useErrors';
 import useInputs from './useInputs';
 import useLocalstorage from './useLocalstorage';
+import usePagination from './usePagination';
 
-export { useCaptcha, useDebounce, useErrors, useInputs, useLocalstorage };
+export { useCaptcha, useDebounce, useErrors, useInputs, useLocalstorage, usePagination };
