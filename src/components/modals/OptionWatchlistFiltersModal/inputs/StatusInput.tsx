cc1916992756f79c353d533a2af48bed0c766249
Ali Yaghoubi <dev.edwardjoseph@gmail.com>
2024-01-15 19:18:20 +0330

    Implement filters

--- src/components/modals/OptionWatchlistFiltersModal/inputs/StatusInput.tsx
+++ src/components/modals/OptionWatchlistFiltersModal/inputs/StatusInput.tsx
@@ -10,39 +10,40 @@ interface StatusInputProps {
 const StatusInput = ({ value, onChange }: StatusInputProps) => {
 	const t = useTranslations();
 
-	const onChangeValue = (v: 'itm' | 'otm' | 'atm') => {
+	const onChangeValue = (v: 'ITM' | 'OTM' | 'ATM') => {
 		if (value.includes(v)) onChange(value.filter((val) => val !== v));
 		else onChange([...value, v]);
 	};
 
-	const isITM = value.includes('itm');
+	const isITM = value.includes('ITM');
 
-	const isOTM = value.includes('otm');
+	const isOTM = value.includes('OTM');
 
-	const isATM = value.includes('atm');
+	const isATM = value.includes('ATM');
 
 	return (
 		<div className='flex-1 gap-8 flex-justify-end *:h-40 *:flex-1 *:gap-8 *:rounded *:border-2 *:font-medium *:transition-colors *:flex-justify-center'>
 			<button
-				onClick={() => onChangeValue('itm')}
+				type='button'
+				onClick={() => onChangeValue('ITM')}
 				className={clsx(isITM ? 'border-link bg-link text-white' : 'border-2 border-link text-link hover:bg-link hover:text-white')}
 			>
 				<span style={{ height: '2px' }} className='w-24 rounded bg-current' />
 				{t('option_watchlist_filters_modal.status_itm')}
 			</button>
 			<button
-				onClick={() => onChangeValue('otm')}
+				type='button'
+				onClick={() => onChangeValue('OTM')}
 				className={clsx(
-					isOTM
-						? 'border-error-100 bg-error-100 text-white'
-						: 'border-2 border-error-100 text-error-100 hover:bg-error-100 hover:text-white',
+					isOTM ? 'border-error-100 bg-error-100 text-white' : 'border-2 border-error-100 text-error-100 hover:bg-error-100 hover:text-white',
 				)}
 			>
 				<ChartDownSVG />
 				{t('option_watchlist_filters_modal.status_otm')}
 			</button>
 			<button
-				onClick={() => onChangeValue('atm')}
+				type='button'
+				onClick={() => onChangeValue('ATM')}
 				className={clsx(
 					isATM
 						? 'border-success-100 bg-success-100 text-white'
