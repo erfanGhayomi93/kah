5b44e3f9962ced405637b668e348280ba0565a1c
Ali Yaghoubi <dev.edwardjoseph@gmail.com>
2024-03-17 09:55:34 +0330

    Complete a part of new version of OptionChain

--- src/components/pages/OptionChain/index.tsx
+++ src/components/pages/OptionChain/index.tsx
@@ -21,7 +21,7 @@ const OptionChain = () => {
 	return (
 		<Main className='gap-8 !px-8'>
 			<Toolbar inputs={inputs} setInputValue={setInputValue} />
-			<Option settlementDay={null} />
+			<Option settlementDay={inputs.settlementDay} baseSymbolISIN={inputs.baseSymbol?.symbolISIN ?? null} />
 		</Main>
 	);
 };
