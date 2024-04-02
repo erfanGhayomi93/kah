03df20a9b4256437850148ea165f3d1e3ea2e6af
Ali Yaghoubi <dev.edwardjoseph@gmail.com>
2024-04-02 14:32:18 +0330

    Bug fix

--- src/features/hooks.ts
+++ src/features/hooks.ts
@@ -1,6 +1,5 @@
-import { type TypedUseSelectorHook, useDispatch, useSelector, useStore } from 'react-redux';
-import type { AppDispatch, AppStore, RootState } from './store';
+import { type TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import type { AppDispatch, RootState } from './store';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-export const useAppStore: () => AppStore = useStore;
