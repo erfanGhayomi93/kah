5ae02689812df1fd57533f298364d9742552e55a
Ali Yaghoubi <dev.edwardjoseph@gmail.com>
2024-04-16 15:22:11 +0330

    Add some features

---
+++ src/app/[locale]/analyze/page.tsx
@@ -0,0 +1,8 @@
+import Analyze from '@/components/pages/Analyze';
+import type { NextPage } from 'next';
+
+const Page: NextPage<INextProps> = async () => {
+	return <Analyze />;
+};
+
+export default Page;
