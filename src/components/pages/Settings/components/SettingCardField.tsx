6b0ef77d86c825397ff53e44107d2de03e5a60f2
Alireza Sedighi <alireza.sedighi.job@gmail.com>
2024-05-12 10:48:10 +0330

    .

--- src/components/pages/Settings/components/SettingCardField.tsx
+++ src/components/pages/Settings/components/SettingCardField.tsx
@@ -1,19 +1,24 @@
+import clsx from 'clsx';
+
 interface IFieldProps {
-	icon: React.ReactNode;
+	icon?: React.ReactNode;
 	title: string | React.ReactNode;
 	node: React.ReactNode;
 	prefixIcon?: React.ReactNode;
+	colon?: boolean;
+	titleClass?: string;
+	valueClass?: string;
 }
 
-const SettingCardField = ({ icon, title, node, prefixIcon }: IFieldProps) => {
+const SettingCardField = ({ icon, title, node, prefixIcon, colon = true, titleClass, valueClass }: IFieldProps) => {
 	return (
 		<div className='flex-justify-between'>
-			<span className='gap-8 text-gray-900 flex-justify-center'>
+			<span className={clsx('gap-8 text-gray-900 flex-justify-center', titleClass)}>
 				{icon}
-				{title + ':'}
+				{title + (colon ? ':' : '')}
 				{prefixIcon && prefixIcon}
 			</span>
-			<span>{node}</span>
+			<span className={valueClass}>{node}</span>
 		</div>
 	);
 };
--- src/components/pages/Settings/tabs/SendOrder/index.tsx
+++ src/components/pages/Settings/tabs/SendOrder/index.tsx
@@ -1,5 +1,52 @@
+'use client'
+import { useTranslations } from 'next-intl';
+import SettingCard from '../../components/SettingCard';
+import SettingCardField from '../../components/SettingCardField';
+import Input from '@/components/modals/BuySellModal/common/Input';
+
 const SendOrder = () => {
-	return <div></div>;
+	const t = useTranslations();
+
+	const fields = [
+		{
+			title: t('settings_page.default_buy_volume'),
+			component: <Input label='asd' onChange={()=> {}} value={0}/>,
+		},
+		{
+			title: t('settings_page.default_sell_volume'),
+			component: <></>,
+		},
+		{
+			title: t('settings_page.confirm_before_send_order'),
+			component: <></>,
+		},
+		{
+			title: t('settings_page.confirm_before_delete_order'),
+			component: <></>,
+		},
+		{
+			title: t('settings_page.Get_online_price'),
+			component: <></>,
+		},
+		{
+			title: t('settings_page.market_watcher_message_notification'),
+			component: <></>,
+		},
+		{
+			title: t('settings_page.show_symbol_info_in_send_order'),
+			component: <></>,
+		},
+	];
+
+	return (
+		<SettingCard title={t('settings_page.send_order_settings')}>
+			<div className='grid grid-cols-2 gap-x-32 gap-y-24'>
+				{fields.map((item) => (
+					<SettingCardField key={item.title} colon={false} title={item.title} node={item.component} />
+				))}
+			</div>
+		</SettingCard>
+	);
 };
 
 export default SendOrder;
