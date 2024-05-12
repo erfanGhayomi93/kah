4c4bdbd93b6881f1e2cc3c4eb41bfb95e4c3a13e
Alireza Sedighi <alireza.sedighi.job@gmail.com>
2024-05-12 12:10:34 +0330

    send order settings finished

--- src/components/pages/Settings/tabs/SendOrder/index.tsx
+++ src/components/pages/Settings/tabs/SendOrder/index.tsx
@@ -1,40 +1,128 @@
-'use client'
+'use client';
+import Input from '@/components/common/Inputs/Input';
+import Switch from '@/components/common/Inputs/Switch';
+import { convertStringToInteger, sepNumbers } from '@/utils/helpers';
 import { useTranslations } from 'next-intl';
+import { useReducer } from 'react';
 import SettingCard from '../../components/SettingCard';
 import SettingCardField from '../../components/SettingCardField';
-import Input from '@/components/modals/BuySellModal/common/Input';
+
+type TReducerType =
+	| 'defaultBuyVolume'
+	| 'defaultSellVolume'
+	| 'confirmSendOrder'
+	| 'confirmDeleteOrder'
+	| 'getOnlinePrice'
+	| 'marketWatcherMessageNotification'
+	| 'showSymbolInfoInSendOrder';
+
+const initialFieldValues = {
+	defaultBuyVolume: '',
+	defaultSellVolume: '',
+	confirmSendOrder: false,
+	confirmDeleteOrder: false,
+	getOnlinePrice: false,
+	marketWatcherMessageNotification: false,
+	showSymbolInfoInSendOrder: false,
+};
+
+const reducer = (
+	state: typeof initialFieldValues,
+	action: { type: TReducerType; payload: string | boolean },
+): typeof initialFieldValues => {
+	return { ...state, [action.type]: action.payload };
+};
 
 const SendOrder = () => {
 	const t = useTranslations();
 
+	const [fieldValues, dispatch] = useReducer(reducer, initialFieldValues);
+
 	const fields = [
 		{
 			title: t('settings_page.default_buy_volume'),
-			component: <Input label='asd' onChange={()=> {}} value={0}/>,
+			component: (
+				<Input
+					classInput='text-right'
+					value={sepNumbers(String(fieldValues.defaultBuyVolume))}
+					onChange={(e) =>
+						dispatch({ type: 'defaultBuyVolume', payload: convertStringToInteger(e.target.value) })
+					}
+					type='text'
+					inputMode='numeric'
+					placeholder='وارد کنید'
+				/>
+			),
 		},
 		{
 			title: t('settings_page.default_sell_volume'),
-			component: <></>,
+			component: (
+				<Input
+					classInput='text-right'
+					value={sepNumbers(String(fieldValues.defaultSellVolume))}
+					onChange={(e) =>
+						dispatch({ type: 'defaultSellVolume', payload: convertStringToInteger(e.target.value) })
+					}
+					type='text'
+					inputMode='numeric'
+					placeholder='وارد کنید'
+				/>
+			),
 		},
 		{
 			title: t('settings_page.confirm_before_send_order'),
-			component: <></>,
+			component: (
+				<>
+					<Switch
+						checked={fieldValues.confirmSendOrder}
+						onChange={(value) => dispatch({ type: 'confirmSendOrder', payload: value })}
+					/>
+				</>
+			),
 		},
 		{
 			title: t('settings_page.confirm_before_delete_order'),
-			component: <></>,
+			component: (
+				<>
+					<Switch
+						checked={fieldValues.confirmDeleteOrder}
+						onChange={(value) => dispatch({ type: 'confirmDeleteOrder', payload: value })}
+					/>
+				</>
+			),
 		},
 		{
 			title: t('settings_page.Get_online_price'),
-			component: <></>,
+			component: (
+				<>
+					<Switch
+						checked={fieldValues.getOnlinePrice}
+						onChange={(value) => dispatch({ type: 'getOnlinePrice', payload: value })}
+					/>
+				</>
+			),
 		},
 		{
 			title: t('settings_page.market_watcher_message_notification'),
-			component: <></>,
+			component: (
+				<>
+					<Switch
+						checked={fieldValues.marketWatcherMessageNotification}
+						onChange={(value) => dispatch({ type: 'marketWatcherMessageNotification', payload: value })}
+					/>
+				</>
+			),
 		},
 		{
 			title: t('settings_page.show_symbol_info_in_send_order'),
-			component: <></>,
+			component: (
+				<>
+					<Switch
+						checked={fieldValues.showSymbolInfoInSendOrder}
+						onChange={(value) => dispatch({ type: 'showSymbolInfoInSendOrder', payload: value })}
+					/>
+				</>
+			),
 		},
 	];
 
