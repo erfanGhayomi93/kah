1fca366675975cabb256832ab847c428cd6b6b8f
Ali Yaghoubi <dev.edwardjoseph@gmail.com>
2024-05-29 15:48:05 +0330

    Very useful improvements

--- src/app/payment-result/page.tsx
+++ src/app/payment-result/page.tsx
@@ -1,7 +1,20 @@
 import PaymentResultPage from '@/components/pages/paymentResultPage';
+import { getMetadata } from '@/metadata';
 
 const Page = () => {
 	return <PaymentResultPage />;
 };
+
+const generateMetadata = () => {
+	return getMetadata({
+		title: 'نتیجه پرداخت',
+		robots: {
+			follow: false,
+			index: false,
+		},
+	});
+};
+
+export { generateMetadata };
 
 export default Page;
