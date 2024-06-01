cf9cc5cb894925b42c3a6ec5215a980d64325f30
Ali Yaghoubi <dev.edwardjoseph@gmail.com>
2024-06-01 13:31:03 +0330

    Add "useReportWebVitals"

---
+++ src/components/common/WebVitals.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable no-console */
+'use client';
+
+import { useReportWebVitals } from 'next/web-vitals';
+
+const WebVitals = () => {
+	useReportWebVitals((metric) => {
+		console.log(metric);
+	});
+
+	return null;
+};
+
+export default WebVitals;
