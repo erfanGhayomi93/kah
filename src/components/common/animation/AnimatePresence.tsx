0227fce3f5186152b8f48fae40c01417f752ff3a
Ali Yaghoubi <dev.edwardjoseph@gmail.com>
2024-06-08 15:53:16 +0330

    Bug fix

--- src/components/common/animation/AnimatePresence.tsx
+++ src/components/common/animation/AnimatePresence.tsx
@@ -32,11 +32,14 @@ const AnimatePresence = ({ exit, initial, children, disabled, onRefLoad }: Anima
 	};
 
 	const toggleAnimation = () => {
-		if (!elRef.current || disabled) return;
+		if (!elRef.current || disabled === true) return;
 
 		const isInitial = Boolean(children);
+		const isDisabled =
+			typeof disabled === 'object' ? (isInitial && disabled.initial) || (!isInitial && disabled.exit) : false;
 		const { animation, duration } = isInitial ? initial : exit;
 
+		if (isDisabled) return;
 		elRef.current.style.animation = `${animation} ${duration ?? 250}ms 1 forwards ease-in-out`;
 	};
 
@@ -51,11 +54,15 @@ const AnimatePresence = ({ exit, initial, children, disabled, onRefLoad }: Anima
 			}
 
 			if (!elRef.current) return;
-
 			toggleAnimation();
 		},
 		[children],
 	);
+
+	const exitOperation = () => {
+		setIsRender(false);
+		setComponent(null);
+	};
 
 	useEffect(() => {
 		const isVisible = Boolean(children);
@@ -66,12 +73,13 @@ const AnimatePresence = ({ exit, initial, children, disabled, onRefLoad }: Anima
 		if (isVisible) {
 			setComponent(children as React.ReactElement);
 			setIsRender(true);
-		} else {
-			debounceRef.current = setTimeout(() => {
-				setIsRender(false);
-				setComponent(null);
-			}, exit.duration ?? 250);
+			return;
 		}
+
+		const isDisabled = disabled === true || (typeof disabled === 'object' && disabled.exit);
+
+		if (isDisabled) exitOperation();
+		else debounceRef.current = setTimeout(exitOperation, exit.duration ?? 250);
 	}, [children]);
 
 	if (!isRender) return null;
