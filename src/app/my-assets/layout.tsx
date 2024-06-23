4bd64e6c95706c78eab3ef6c9892c1c78fff275d
Ali Yaghoubi <dev.edwardjoseph@gmail.com>
2024-06-23 11:51:18 +0330

    Update page titles

--- src/app/my-assets/layout.tsx
+++ src/app/my-assets/layout.tsx
@@ -1,7 +1,5 @@
 import Main from '@/components/layout/Main';
 import Toolbar from '@/components/pages/MyAssets/Toolbar';
-import { getMetadata } from '@/metadata';
-import { getTranslations } from 'next-intl/server';
 
 const Layout = ({ children }: { children: ReactNode }) => (
 	<Main className='gap-8 !px-8'>
@@ -9,15 +7,5 @@ const Layout = ({ children }: { children: ReactNode }) => (
 		{children}
 	</Main>
 );
-
-const generateMetadata = async () => {
-	const t = await getTranslations('meta_title');
-
-	return getMetadata({
-		title: t('my_assets_stocks'),
-	});
-};
-
-export { generateMetadata };
 
 export default Layout;
--- src/app/my-assets/position/page.tsx
+++ src/app/my-assets/position/page.tsx
@@ -1,8 +1,20 @@
 import Position from '@/components/pages/MyAssets/Position';
+import { getMetadata } from '@/metadata';
 import { type NextPage } from 'next';
+import { getTranslations } from 'next-intl/server';
 
 const Page: NextPage<INextProps> = () => {
 	return <Position />;
 };
+
+const generateMetadata = async () => {
+	const t = await getTranslations('meta_title');
+
+	return getMetadata({
+		title: t('my_assets_position'),
+	});
+};
+
+export { generateMetadata };
 
 export default Page;
