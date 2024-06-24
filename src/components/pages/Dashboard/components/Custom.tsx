3ec8c3a4829f146a7f8bd9d411e381f8e078fb7c
Ali Yaghoubi <dev.edwardjoseph@gmail.com>
2024-06-24 12:31:52 +0330

    Update dashboard layout and pair algorithm

--- src/components/pages/Dashboard/components/Custom.tsx
+++ src/components/pages/Dashboard/components/Custom.tsx
@@ -1,6 +1,7 @@
 import { initialDashboardLayout } from '@/constants';
-import { useAppDispatch } from '@/features/hooks';
+import { useAppDispatch, useAppSelector } from '@/features/hooks';
 import { setManageColumnsModal } from '@/features/slices/modalSlice';
+import { getDashboardGridLayout, setDashboardGridLayout } from '@/features/slices/uiSlice';
 import { useTranslations } from 'next-intl';
 
 const Plus = () => (
@@ -23,16 +24,61 @@ const Plus = () => (
 const Custom = () => {
 	const t = useTranslations('home');
 
+	const gridLayout = useAppSelector(getDashboardGridLayout);
+
 	const dispatch = useAppDispatch();
 
+	const getSectionVisibilities = () => {
+		const result: Partial<Record<TDashboardSections, boolean>> = {};
+
+		try {
+			for (let i = 0; i < gridLayout.length; i++) {
+				const item = gridLayout[i];
+				result[item.id] = item.hidden;
+			}
+		} catch (e) {
+			//
+		}
+
+		return result;
+	};
+
+	const onColumnChanged = (cols: Array<IManageColumn<TDashboardSections>>) => {
+		try {
+			const result: Partial<Record<TDashboardSections, boolean>> = {};
+
+			for (let i = 0; i < cols.length; i++) {
+				const col = cols[i];
+				result[col.id] = col.hidden;
+			}
+
+			dispatch(
+				setDashboardGridLayout(
+					gridLayout.map((s) => ({
+						...s,
+						hidden: result[s.id] ?? false,
+					})),
+				),
+			);
+		} catch (e) {
+			//
+		}
+	};
+
 	const openDashboardLayoutManager = () => {
+		const columnsVisibility = getSectionVisibilities();
+		const columns = initialDashboardLayout.map((col) => ({
+			...col,
+			hidden: columnsVisibility[col.id] ?? false,
+		}));
+
 		dispatch(
 			setManageColumnsModal({
-				columns: initialDashboardLayout,
+				columns,
 				title: t('manage_layout'),
 				initialColumns: initialDashboardLayout,
 				stream: false,
-				onColumnChanged: console.log,
+				onColumnChanged: (cols) => onColumnChanged(cols as Array<IManageColumn<TDashboardSections>>),
 			}),
 		);
 	};
