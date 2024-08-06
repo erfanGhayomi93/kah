cefd34ea029baed8913dbcf5e17152ae1474ff33
Ali Yaghoubi <dev.edwardjoseph@gmail.com>
2024-08-06 16:41:22 +0330

    Bug fix

--- src/components/pages/MyAssets/Stocks/Table.tsx
+++ src/components/pages/MyAssets/Stocks/Table.tsx
@@ -224,14 +224,6 @@ const Table = ({ data, loading }: TableProps) => {
 				columnDefs={columnDefs}
 				defaultColDef={defaultColDef}
 				className='h-full border-0'
-				quickFilterMatcher={(value, rawText) => {
-					try {
-						const symbolTitle = rawText.split('\n')[1];
-						return symbolTitle.includes(value[0]);
-					} catch (e) {
-						return true;
-					}
-				}}
 				onSortChanged={({ api }) => {
 					const column = api.getColumn('index');
 					if (!column) return;
