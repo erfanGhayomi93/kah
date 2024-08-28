c6ebbcd66b8804d45e6e5fd8ff0a736c71b33f6a
Ali Yaghoubi <dev.edwardjoseph@gmail.com>
2024-08-28 15:04:06 +0330

    Update ls messages queue

--- src/classes/OMSGateway/OMSMessagesQueue.tsx
+++ src/classes/OMSGateway/OMSMessagesQueue.tsx
@@ -8,17 +8,23 @@ type TResource = Record<string, string>;
 class OMSMessagesQueue {
 	private readonly _queue = new Map<string, TMessage>();
 
+	private _count: Record<string, number> = {};
+
 	private _orderStatus: TResource = {};
 
 	private _orderErrors: TResource = {};
 
+	private _debounce: NodeJS.Timeout | null = null;
+
 	private readonly _delay = 1000;
+
+	private readonly _closeDuration = 2500;
 
 	constructor() {
 		this._loadResources();
 	}
 
-	add(message: TMessage) {
+	public add(message: TMessage) {
 		const clientKey = this._getClientKey(message);
 
 		if (!this._queue.has(clientKey)) {
@@ -40,7 +46,7 @@ class OMSMessagesQueue {
 	private _showMessage(message: TMessage) {
 		// 208 ? 208 : 22 + 200
 
-		const clientKey = message[12];
+		// const clientKey = message[12];
 		const orderStatus = message[22];
 		const orderMessageType = message[200];
 		const orderMessage = message[208];
@@ -58,16 +64,33 @@ class OMSMessagesQueue {
 			else messageText = orderStatusMessage;
 		}
 
-		if (toast.isActive(clientKey)) {
-			toast.update(messageText, {
-				autoClose: 3500,
-				toastId: clientKey,
-				type: messageType,
+		const toastId = orderMessageType || orderStatus;
+
+		if (toast.isActive(toastId)) {
+			if (!(toastId in this._count)) this._count[toastId] = 1;
+			this._count[toastId] += 1;
+
+			toast.update(toastId, {
+				toastId,
+				autoClose: this._closeDuration,
+				render: (
+					<div className='w-full gap-8 flex-items-center'>
+						<span className='flex-1'>{messageText}</span>
+						<span style={{ flexBasis: '24px' }} className='shrink-0 grow-0'>
+							({this._count[toastId] ?? 2})
+						</span>
+					</div>
+				),
 			});
+
+			this._clearDebounce();
+			this._debounce = setTimeout(() => {
+				delete this._count[toastId];
+			}, this._closeDuration);
 		} else {
 			toast[messageType](messageText, {
-				autoClose: 3500,
-				toastId: clientKey,
+				toastId,
+				autoClose: this._closeDuration,
 			});
 		}
 
@@ -88,6 +111,10 @@ class OMSMessagesQueue {
 
 	private _getClientKey(message: TMessage) {
 		return message[12];
+	}
+
+	private _clearDebounce() {
+		if (this._debounce) clearTimeout(this._debounce);
 	}
 }
 
